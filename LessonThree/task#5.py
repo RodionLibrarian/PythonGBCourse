5103fbc15041bca693a13af5fc3f1b966fd7c9ac
rodion-batiev-pro <rodion.batiev@ntprog.com>
2020-09-25 03:37:28 +0300

    [LessonThree] task#5.py update commit

--- LessonThree/task#5.py
+++ LessonThree/task#5.py
@@ -1,11 +1,16 @@
-result = 0
-while True:
-    list = input('Введите числа через пробел или "." для выхода: ').split()
-    for i in range(len(list)):
-        if list[i] == '.':
-            print(result)
-            quit()
-        else:
-            result += int(list[i])
-    print(result)
+def my_func():
+    result = 0
+    while True:
+        list = input('Введите числа через пробел или "." для выхода: ').split()
+        for i in range(len(list)):
+            if list[i] == '.':
+                return result
+            else:
+                try:
+                    result += int(list[i])
+                except ValueError:
+                    print('Введите числа или "."!')
+        print(result)
 
+
+print(my_func())
