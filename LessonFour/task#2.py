9ce02036afef23ddc172a8a1efaf561206922e47
rodion-batiev-pro <rodion.batiev@ntprog.com>
2020-09-30 20:46:42 +0300

    [LessonFour] task#2.py fix update

--- LessonFour/task#2.py
+++ LessonFour/task#2.py
@@ -1,5 +1,5 @@
 list = [109, 234, 532, 32, 43, 12, 54, 54, 23, 12]
-new_list = [el for num, el in enumerate(list) if list[num] > list[num - 1]]
+new_list = [fnum for fnum, snum in zip(list[1:], list[:-1]) if fnum > snum]
 
 print(f'Исходный список {list}')
 print(f'Новый список {new_list}')
