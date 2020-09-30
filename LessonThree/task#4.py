76f337872e0aff6aadd299b9a65c977303277e4a
rodion-batiev-pro <rodion.batiev@ntprog.com>
2020-09-30 17:22:31 +0300

    [LessonThree] task#4.py fix commit

--- LessonThree/task#4.py
+++ LessonThree/task#4.py
@@ -2,13 +2,14 @@ def my_func(x, y):
     return 1 / x ** abs(y)
 
 
-print(my_func(2.4, -2))
+print(my_func(2, -4))
 
 
 def my_func2(x, y):
-    for i in range(abs(y)-1):
-        x *= x
-    return 1 / x
+    result = 1
+    for i in range(abs(y)):
+        result *= x
+    return 1 / result
 
 
-print(my_func2(2.4, -2))
+print(my_func2(2, -4))
