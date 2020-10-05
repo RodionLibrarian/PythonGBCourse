7b119213a6cc08954fc619c8776bda469cd08058
rodion-batiev-pro <rodion.batiev@ntprog.com>
2020-10-05 17:23:28 +0300

    [LessonEight] Update file commit

--- LessonEight/task#7.py
+++ LessonEight/task#7.py
@@ -5,19 +5,19 @@ class ComplexNumber:
         self.z = 'a + b * i'
 
     def __add__(self, other):
-        print(f'Сумма z1 и z2 равна')
+        print(f'Z1 + Z2 is equal: ')
         return f'z = {self.a + other.a} + {self.b + other.b} * i'
 
     def __mul__(self, other):
-        print(f'Произведение z1 и z2 равно')
+        print(f'Z1 * Z2 is equal: ')
         return f'z = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
 
     def __str__(self):
         return f'z = {self.a} + {self.b} * i'
 
 
-z_1 = ComplexNumber(1, -2)
-z_2 = ComplexNumber(3, 4)
+z_1 = ComplexNumber(3, -4)
+z_2 = ComplexNumber(3, -6)
 print(z_1)
 print(z_2)
 print(z_1 + z_2)
